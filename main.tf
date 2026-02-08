d14932f8a618d8ee092a88ebd9db0dc5728fee48
naadirlab <sharifnaadir@gmail.com>
2026-02-08 11:07:25 +0100

    add Wordpress instance and SGs

---
+++ main.tf
@@ -0,0 +1,47 @@
+# EC2 Instance Configurations 
+
+resource "aws_instance" "wordpress_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
+  
+  user_data = <<EOF
+#!/bin/bash
+yum update -y
+amazon-linux-extras install php8.0 -y
+yum install -y httpd mysql
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+chown -R apache:apache wordpress
+systemctl restart httpd
+EOF
+}
+
+
+# Security Groups
+
+resource "aws_security_group" "wordpress_sg" {
+ ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # Allow HTTP Traffic
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # Allow HTTPS Traffic
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
